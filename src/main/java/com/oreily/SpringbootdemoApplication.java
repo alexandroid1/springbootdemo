af3dfa2b1cced75ac0318e30ad2a5c3b8d67020e
Alexksandr <archangel7017@gmail.com>
2017-09-25 19:24:51 +0300

    rest controller

--- src/main/java/com/oreily/SpringbootdemoApplication.java
+++ src/main/java/com/oreily/SpringbootdemoApplication.java
@@ -1,12 +1,25 @@
 package com.oreily;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+//@SpringBootApplication
+
+@RestController
+@EnableAutoConfiguration
 public class SpringbootdemoApplication {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
+
 }
