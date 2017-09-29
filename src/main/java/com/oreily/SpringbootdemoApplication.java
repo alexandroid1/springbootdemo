0f5fae9d4b4b73bbf65690f536d2a8b99c8383d8
Alexksandr <archangel7017@gmail.com>
2017-09-29 16:46:33 +0300

    yml configuration

--- src/main/java/com/oreily/SpringbootdemoApplication.java
+++ src/main/java/com/oreily/SpringbootdemoApplication.java
@@ -1,22 +1,26 @@
 package com.oreily;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
-
 @RestController
 @SpringBootApplication // В данном случае аннотация @SpringBootApplication заменяет собой аннотации @Configuration, @EnableAutoConfiguration и @ComponentScan.
+@Import(MyConfiguration.class)
+
 //@EnableAutoConfiguration
 public class SpringbootdemoApplication {
 
+	@Value("${name}")
+	String name;
+
 	@RequestMapping("/")
 	public String home(){
-		return "Hello World";
+		return name;
 	}
 
 	public static void main(String[] args) {
