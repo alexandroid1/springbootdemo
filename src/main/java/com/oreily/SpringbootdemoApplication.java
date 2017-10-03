6c053c807a160f489358f89d2cef50cef88fb564
Alexksandr <archangel7017@gmail.com>
2017-10-03 17:28:11 +0300

    SpringBootApplication annotation only

--- src/main/java/com/oreily/SpringbootdemoApplication.java
+++ src/main/java/com/oreily/SpringbootdemoApplication.java
@@ -1,26 +1,29 @@
 package com.oreily;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication // В данном случае аннотация @SpringBootApplication заменяет собой аннотации @Configuration, @EnableAutoConfiguration и @ComponentScan.
-@Import(MyConfiguration.class)
 
-//@EnableAutoConfiguration
 public class SpringbootdemoApplication {
 
 	@Value("${name}")
 	String name;
 
+	@Autowired
+	String message;
+
 	@RequestMapping("/")
 	public String home(){
-		return name;
+		return message;
 	}
 
 	public static void main(String[] args) {
