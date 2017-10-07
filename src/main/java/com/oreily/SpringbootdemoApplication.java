567511ab846c93c6973ecc56e0bdbaae6c128769
Alexksandr <archangel7017@gmail.com>
2017-10-07 17:17:03 +0300

    ApplicationRunner added

--- src/main/java/com/oreily/SpringbootdemoApplication.java
+++ src/main/java/com/oreily/SpringbootdemoApplication.java
@@ -2,6 +2,7 @@ package com.oreily;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,17 +12,24 @@ import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication // В данном случае аннотация @SpringBootApplication заменяет собой аннотации @Configuration, @EnableAutoConfiguration и @ComponentScan.
+@RestController
 //@EnableConfigurationProperties(value = MyMessage.class)
 public class SpringbootdemoApplication {
+
+	@Autowired
+	ApplicationArguments applicationArguments;
 
 	@Autowired
 	private MyMessage myMessage;
 
 	@RequestMapping("/")
 	public String welcome(){
-		return "welcome, your lucky number is " + myMessage.getMessageValue();
+		String value = applicationArguments.getOptionNames().iterator().next();
+
+		//return "welcome, your lucky number is " + myMessage.getMessageValue();
+
+		return "welcome, your lucky number is " + value;
 	}
 
 
